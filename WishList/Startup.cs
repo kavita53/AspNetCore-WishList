b5614e2beebbafb2d036f2c547bd88ebac72f5bb
vmadmin <vmadmin@vssql>
2020-09-24 14:50:00 +0000

    added appMvc service in startup

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -17,6 +17,8 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +28,9 @@ namespace WishList
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+
+            app.UseMvc();
         }
+
     }
 }
