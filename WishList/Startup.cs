015af4eefdc76f0c00565e3a81064347b3e256d3
vmadmin <vmadmin@vssql>
2020-09-25 05:55:12 +0000

    added exception handelling

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,7 +23,7 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if(env.IsDevelopement)
+            if(env.IsDevelopement())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -31,8 +31,8 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseRouting();
-            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseMvcWithDefaultRoute();
+            
             
         }
 
